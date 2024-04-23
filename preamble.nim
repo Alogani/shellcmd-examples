c9d5ece9120b29302e0cecf8465bfbdec6ef796a
Alogani <77880767+Alogani@users.noreply.github.com>
2024-04-23 08:32:51 +0200

    Create preamble.nim

---
+++ preamble.nim
@@ -0,0 +1,15 @@
+#[
+  version       = "0.1.0"
+  author        = "Anonymous"
+  description   = "What your package do"
+  license       = "MIT"
+
+  requires "shellcmd = 0.1.0"
+]#
+
+#[ The following fields are mandatory :
+  * version
+  * description
+  * requires with shellcmd version used
+  * requires for external dependencies
+]#
